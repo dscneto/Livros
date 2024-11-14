c6993f8f7c8f8487a75f4986b921af4c4875fe32
dscneto <46493056+dscneto@users.noreply.github.com>
2024-11-14 15:12:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Meus Livros</h1>
+      <ul>
+        <li><a href="https://github.com/usuario/meus-livros/raw/main/livros/titulo-do-livro.pdf" download>Título do Livro</a></li>
+        <li><a href="https://github.com/usuario/meus-livros/raw/main/livros/outro-livro.pdf" download>Outro Livro</a></li>
+      </ul>
+    </div>
+  )
+}
